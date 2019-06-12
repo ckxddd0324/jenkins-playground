64f0e8e3bd5385be510ab4470dc586dadc37d774
Cheuk Chan <cheukkwan_chan@outlook.comm>
2019-06-12 00:02:27 -0400

    fix the typo

--- test_sum.py
+++ test_sum.py
@@ -5,7 +5,7 @@ from sum import *
 class TestSum(unittest.TestCase):
     
     def test_sum_1(self):
-        self.assertEqual(add_numbers(1,2), 3, "Expected sum of 1 and 2 to be 3 bit got " + str(add_number(1, 2)))
+        self.assertEqual(add_numbers(1,2), 3, "Expected sum of 1 and 2 to be 3 bit got " + str(add_numbers(1, 2)))
     
     def test_sum_2(self):
         self.assertEqual(add_numbers(10, 20), 30)
